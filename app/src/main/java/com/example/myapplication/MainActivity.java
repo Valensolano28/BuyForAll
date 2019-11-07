70bcd3857fa18f765669d87dc49c4337ae0521c3
Administrator <nenene45@naver.com>
2019-11-07 17:59:16 +0900

    19/11/07

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.myapplication;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+
+    private static final String ACCESS_FINE_LOCATION = "2";
+    private BeaconManager beaconManager;
+    private int int_count = 0;
+    // 감지된 비콘들을 임시로 담을 리스트
+    private List<Beacon> beaconList = new ArrayList<>();
+    TextView textView;
+    TextView textcount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this,
+                new String[]{ACCESS_FINE_LOCATION,
+                        ACCESS_COARSE_LOCATION}, 2);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        textView = (TextView)findViewById(R.id.textView);
+        textcount = (TextView)findViewById(R.id.count);
+
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+
+        // 비콘 탐지를 시작한다. 실제로는 서비스를 시작하는것.
+        beaconManager.bind(this);
+        handler.sendEmptyMessage(0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            // 비콘이 감지되면 해당 함수가 호출된다. Collection<Beacon> beacons에는 감지된 비콘의 리스트가,
+            // region에는 비콘들에 대응하는 Region 객체가 들어온다.
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                    List<Beacon> tempList = new ArrayList<>();
+                    int_count = beacons.size();
+                    beaconList.clear();
+                    beaconList = (List)beacons;
+            }
+        });
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+
+               for(Beacon beacon : beaconList){
+                  // String count = "beacon:"+beaconList.size();
+                   String count  = "beacon:" + int_count;
+                   textcount.setText(count);
+                   if(beaconList.size() == 0){
+                       textView.setText("");
+                   }else{
+
+                       String beaconinfo = "Name:" + beacon.getBluetoothName() + "\nRSSI:" + beacon.getRssi() + "\nUUID:" + beacon.getId1() + " \nMajor:" + beacon.getId2() +
+                               "\nMinor:" + beacon.getId3() + " \nDistance:" + Double.parseDouble(String.format("%.3f", beacon.getDistance())) + "m\n";
+                       textView.setText(beaconinfo);
+                   }
+            }
+            handler.sendEmptyMessageDelayed(0, 1000); //1초마다 호출
+        }
+    };
+}
+
+
+
+
