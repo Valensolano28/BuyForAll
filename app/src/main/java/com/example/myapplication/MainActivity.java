d6c52fafbdf219af2d42944cefe113ac68c3db63
nenene45 <nenene45@naver.com>
2019-11-11 02:28:51 +0900

    11/11

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,19 +6,14 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.RemoteException;
 import android.util.Log;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
-import com.axaet.ibeacon.A_BluetoothLeService;
-
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
@@ -31,24 +26,22 @@ import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     private static final String ACCESS_FINE_LOCATION = "2";
     private BeaconManager beaconManager;
-    private int int_count = 0;
     private List<Beacon> beaconList = new ArrayList<>();
     private TextView textView;
-    private TextView textcount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
         ActivityCompat.requestPermissions(this,
                 new String[]{ACCESS_FINE_LOCATION,
                         ACCESS_COARSE_LOCATION}, 2);
+
         beaconManager = BeaconManager.getInstanceForApplication(this);
-        textView = findViewById(R.id.textView);
-        textcount = findViewById(R.id.count);
-
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-
         beaconManager.bind(this);
         handler.sendEmptyMessage(0);
     }
@@ -61,39 +54,33 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
     @Override
     public void onBeaconServiceConnect() {
-
-        beaconManager.setRangeNotifier(new RangeNotifier() {
-            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-               // List<Beacon> tempList = new ArrayList<>();
-                int_count = beacons.size();
-                //beaconList.clear();
-                beaconList = (List)beacons;
-            }
-        });
         try {
             beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
         } catch (RemoteException e) {
             e.printStackTrace();
         }
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                beaconList.clear();
+                beaconList = (List)beacons;
+            }
+        });
     }
 
     Handler handler = new Handler() {
         public void handleMessage(Message msg) {
                for(Beacon beacon : beaconList){
-                   String count  = "beacon:" + int_count;
-
-                   textcount.setText(count);
                    if(beaconList.size() == 0){
                        textView.setText("");
                    }else{
-                        Log.e("",""+beacon.getBeaconTypeCode() + " " + beacon.getManufacturer());
+                       Log.e("beacon","TypeCode"+beacon.getBeaconTypeCode() + " " + beacon.getManufacturer());
                        String beaconinfo = "Name:" + beacon.getBluetoothName() + "\nRSSI:" + beacon.getRssi() + "\nUUID:" + beacon.getId1() + " \nMajor:" + beacon.getId2() +
-                               "\nMinor:" + beacon.getId3() + " \nDistance:" + Double.parseDouble(String.format("%.3f", beacon.getDistance())) + "m\n" + beacon.getBeaconTypeCode();
-                       textView.setText(beaconinfo);;
+                                         "\nMinor:" + beacon.getId3() + " \nDistance:" + String.format("%.3f", beacon.getDistance());
+                       textView.setText(beaconinfo);
                    }
-            }
-            handler.sendEmptyMessageDelayed(0, 1000); //1초마다 호출
+               }
+            handler.sendEmptyMessageDelayed(0, 1000);
         }
     };
 }
