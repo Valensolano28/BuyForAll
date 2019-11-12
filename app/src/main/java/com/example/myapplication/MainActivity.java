46b19c10d91503df2763e0b9576c82fc6720b2e5
Administrator <nenene45@naver.com>
2019-11-12 17:30:10 +0900

    19/11/12

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.myapplication;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -13,30 +10,28 @@ import android.os.Message;
 import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.BeaconTransmitter;
-import org.altbeacon.beacon.Identifier;
-import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
-import java.lang.reflect.Array;
+import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
@@ -46,25 +41,76 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     private BeaconManager beaconManager;
     private List<Beacon> beaconList = new ArrayList<>();
     private TextView textView;
-    private final int Int_Major = 10010;
-    private final int Int_Minor = 54488;
+    private final int Int_Major1 = 10010;
+    private final int Int_Minor1 = 54488;
+    private final int Int_Major2 = 10004;
+    private final int Int_Minor2 = 54480;
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private Button onbutton;
+    private Button offbutton;
+    private List<String> list = new ArrayList();
     private final Region region = new Region("myRangingUniqueId", null, null, null);
+    private String g_ad = "google 광고";
+    private String d_ad = "daum 광고";
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
+        onbutton = findViewById(R.id.onbutton);
+        offbutton = findViewById(R.id.offbutton);
 
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
         ActivityCompat.requestPermissions(this,
                 new String[]{ACCESS_FINE_LOCATION,
                         ACCESS_COARSE_LOCATION}, 2);
 
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-        beaconManager.bind(this);
+
+        onbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                beaconManager.bind(MainActivity.this);
+            }
+        });
+        offbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    beaconManager.stopMonitoringBeaconsInRegion(region);
+                    beaconManager.stopRangingBeaconsInRegion(region);
+                } catch (RemoteException e) { e.printStackTrace(); }
+                beaconManager.unbind(MainActivity.this);
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String temp = list.get(position);
+                if(temp.equals(g_ad)){
+                    dialog(temp);
+                }else if(temp.equals(d_ad)){
+                    dialog(temp);
+                }else{
+                    dialog(temp);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+        finish();
     }
 
     @Override
@@ -78,6 +124,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         super.onStop();
         try {
             beaconManager.stopRangingBeaconsInRegion(region);
+            beaconManager.stopMonitoringBeaconsInRegion(region);
         } catch (RemoteException e) { e.printStackTrace(); }
         beaconManager.unbind(this);
     }
@@ -96,6 +143,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
                     beaconList = (List) beacons;
 
                     Log.e("beaconList", beacons.toString());
+                    list.clear();
                     handler.sendEmptyMessage(0);
                 } else
                     Log.e("No_beaconList", beacons.toString());
@@ -103,46 +151,80 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         });
     }
 
-    Handler find_beacon = new Handler() {
-        public void handleMessage(Message msg) {
-            try {
-                beaconManager.stopRangingBeaconsInRegion(region);
-                beaconManager.unbind(MainActivity.this);
-            } catch (RemoteException e) { e.printStackTrace(); }
-            Log.e("handle", "startActivity");
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
-            startActivity(intent);
-            onStop();
-        }
-    };
+    public void dialog(String msg) {
+
+        String g_url = "http://www.google.com";
+        String d_url = "http://www.daum.net";
+        String n_url = "http://www.naver.com";
+        final String url;
+        if(msg.equals(g_ad)){
+           url = g_url;
+        }else if(msg.equals(d_ad))
+            url = d_url;
+        else
+            url = n_url;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(msg);
+        builder.setMessage(msg + " 연결?");
+        builder.setPositiveButton("예",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                    }
+                });
+        builder.setNegativeButton("아니오",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"아니오를 선택했습니다.",Toast.LENGTH_LONG).show();
+                    }
+                });
+        builder.show();
+    }
 
         Handler handler = new Handler() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             public void handleMessage(Message msg) {
-                for (Beacon beacon : beaconList) {
-                    String NAME = "Name:" + beacon.getBluetoothName();
-                    String RSSI = "\nRSSI:" + beacon.getRssi();
-                    String UUID = "\nUUID:" + beacon.getId1();
-                    String Major = "\nMajor:" + beacon.getId2();
-                    String Minor = "\nMinor:" + beacon.getId3();
-                    String Distance = "\nDistance:" + String.format("%.3f", beacon.getDistance()) + "m";
-                    String temp_major = Major.split(":")[1];
-                    String temp_minor = Minor.split(":")[1];
-                    if (Int_Major == Integer.parseInt(temp_major) && Int_Minor == Integer.parseInt(temp_minor)) {
-                        //Log.i("beacon", "TypeCode" + beacon.getBeaconTypeCode() + " " + beacon.getManufacturer());
-                        String beaconinfo = NAME + RSSI + UUID + Major + Minor + Distance;
-                        if (beacon.getDistance() <= 1) {
-                            Log.e("beacon", "비콘 들어옴");
-                            Toast.makeText(MainActivity.this, "비콘 들어옴(" + beacon.getBluetoothName() +" "+ String.format("%.3f", beacon.getDistance()) + "m)", Toast.LENGTH_SHORT).show();
-                            beaconinfo += "\n비콘들어옴" + beacon.getBluetoothName();
-                            find_beacon.sendEmptyMessage(1);
-                        } else
-                            Log.i("beacon","비콘 찾음(" + beacon.getBluetoothName() + ")");
-                        textView.setText(beaconinfo);
+                String beaconinfo;
+                String temp_beaconinfo ="";
+                if(beaconList.size() > 0){
+
+                    textView.setText("");
+                    for (int i = 0; i < beaconList.size(); i++) {
+                        Beacon beacon = beaconList.get(i);
+                        String NAME = "Name:" + beacon.getBluetoothName();
+                        String RSSI = "\nRSSI:" + beacon.getRssi();
+                        String UUID = "\nUUID:" + beacon.getId1();
+                        String Major = "\nMajor:" + beacon.getId2();
+                        String Minor = "\nMinor:" + beacon.getId3();
+                        String Distance = "\nDistance:" + String.format("" + "%.3f", beacon.getDistance()) + "m\n ";
+                        String temp_major = Major.split(":")[1];
+                        String temp_minor = Minor.split(":")[1];
+                        beaconinfo = NAME + RSSI + UUID + Major + Minor + Distance;
+
+                        if (Int_Major1 == Integer.parseInt(temp_major) && Int_Minor1 == Integer.parseInt(temp_minor)) {
+                            list.add(g_ad);
+                            if (beacon.getDistance() <= 1) {
+                                //거리 1m이하일때
+                                //Log.e("beacon", "비콘 1m안에 들어옴");
+                                //Toast.makeText(MainActivity.this, "비콘 들어옴(" + beacon.getBluetoothName() +" "+ String.format("%.3f", beacon.getDistance()) + "m)", Toast.LENGTH_SHORT).show();
+                                //beaconinfo += "\n비콘들어옴" + beacon.getBluetoothName();
+                                //find_beacon.sendEmptyMessage(1);
+                            }
+
+                        }else if(Int_Major2 == Integer.parseInt(temp_major) && Int_Minor2 == Integer.parseInt(temp_minor)) {
+                            list.add(d_ad);
+                        }else{
+                            list.add(beacon.getBluetoothName());
+                        }
+                        Log.i("beacon", "ListAdd "+ NAME + " " + temp_major +" " + temp_minor);
+                        temp_beaconinfo += beaconinfo;
                     }
+                    textView.append(temp_beaconinfo);
                     beaconList.clear();
-                }
-               // handler.sendEmptyMessageDelayed(0, 1000);
             }
+            }
+
         };
 }
